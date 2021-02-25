867c2e8625c2331b73f12b34754c9b8496e04391
nlmuscanell <nicolemuscanell@gmail.com>
2021-02-24 22:10:11 -0500

    commit

---
+++ index.py
@@ -0,0 +1,50 @@
+# Import Dependencies
+from flask import Flask, render_template, request, redirect, flash, url_for
+import main
+import urllib.request
+from werkzeug.utils import secure_filename
+from main import getPrediction
+import os
+
+
+#################################################
+# Flask Setup
+#################################################
+
+UPLOAD_FOLDER = '/Users/nicolemuscanell/UCF-FINAL-PROJECT/static/'
+
+app = Flask(__name__)                    
+app.secret_key = '8662747133'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Route to HTML    
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route("/", methods = ['POST']) #/file
+# Our function for pushing the image to the classifier model
+def submit_image():
+     if request.method == 'POST':
+          if 'file' not in request.files:
+               flash('No file part')
+               return redirect(request.url)
+          file = request.files['file']
+        # Error message if no file submitted
+          if file.filename == '':
+            flash('No file selected for uploading')
+            return redirect(request.url)
+        # Return results predictive data
+          if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join('/Users/nicolemuscanell/UCF-FINAL-PROJECT/static/', filename))
+            getPrediction(filename)
+            answer, probability_results, filename = getPrediction(filename)
+            flash(answer)
+            flash(probability_results) # accuracy
+            flash(filename)
+            return redirect('/')
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ main.py
@@ -0,0 +1,35 @@
+# Dependencies
+import numpy as np
+import keras
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.applications.vgg16 import preprocess_input
+from keras.applications.vgg16 import decode_predictions
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from keras.applications.vgg16 import VGG16
+import tensorflow as tf
+
+# def predict(img_path):
+def getPrediction(filename):
+     model = tf.keras.models.load_model("/Users/nicolemuscanell/UCF-FINAL-PROJECT/Resources/Model/final_model_weights.hdf5")
+     img = load_img('/Users/nicolemuscanell/UCF-FINAL-PROJECT/static/'+filename, target_size=(180, 180))
+     img = img_to_array(img)
+     img = img / 255
+     img = np.expand_dims(img,axis=0)
+     category = model.predict_classes(img)
+     answer = category[0]
+     probability = model.predict(img)
+     probability_results = 0
+
+     if answer == 1:
+          answer = "Recycle"
+          probability_results = probability[0][1]
+     else:
+          answer = "Organic"
+          probability_results = probability[0][0]
+
+     answer = str(answer)
+     probability_results=str(probability_results)
+
+     values = [answer, probability_results, filename]
+     return values[0], values[1], values[2]
